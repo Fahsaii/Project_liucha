88f8daee555c600d8be993e1a2e5458d7b42b126
fahsaii <fahsai.tpd@gmail>
2025-03-06 02:30:11 +0700

    ..

--- index.php
+++ index.php
@@ -88,6 +88,19 @@ $isAdmin = isset($_SESSION['role']) && $_SESSION['role'] === 'admin';
             </div>
         <?php endwhile; ?>
     </div>
+    <div class="topping-title">TOPPING!</div>
+    <div class="topping-container">
+        <div class="topping-item">
+            <img src="image/chocmint_new.jpg" alt="chocmint_new">
+        </div>
+        <div class="topping-item">
+            <img src="image/caramel_new.jpg" alt="Caramel Milk">
+        </div>
+        <div class="topping-item">
+            <img src="image/Rose_new.jpg" alt="Rose Milk Tea">
+        </div>
+    </div>      
+
     <div class="menunew-title">NEW!</div>
     <div class="menunew-container">
         <div class="menunew-item">
